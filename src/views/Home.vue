eb84825eb43bdc051c4c57314f6204ae27a585ee
jicf <jicf@zylliondata.com>
2019-05-22 12:35:13 +0800

    临时提交

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="home">
+  <div class="home" data-style="display: flex;">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Vue from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default Vue.extend({
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld,
-  },
+    HelloWorld
+  }
 });
 </script>
 
